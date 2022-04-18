c12c0aa4ff6d22205ea1723dbe030170c3faf002
Mabranche <ngansopangegabrielle@gmail.com>
2022-04-18 10:39:40 +0100

    --- Auto-Git Commit ---

--- app/Http/Controllers/postcontroller.php
+++ app/Http/Controllers/postcontroller.php
@@ -15,7 +15,9 @@ class PostController extends Controller
         ];
 
 
-        return view('articles', compact('post '));
+        return view('articles', [
+            'posts' => $posts
+        ]);
     }
 
     public function show($id)
