344e9b763038002f381a69fc1b5687e55409dea7
Mabranche <ngansopangegabrielle@gmail.com>
2022-04-28 23:59:01 +0100

    Add factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,13 +2,23 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\post>
  */
 class PostFactory extends Factory
+
 {
+    /**
+     * the name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model  = Post::class;
+
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +27,9 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title'  => $this->faker->sentence,
+            'content' => $this->faker->paragraph,
+            'created_at'  =>  now()
         ];
     }
 }
