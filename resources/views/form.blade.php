111cb50cfe2eadd75ca35d4c7f4e18990f24ef4f
Mabranche <ngansopangegabrielle@gmail.com>
2022-05-19 12:01:38 +0100

    --- Auto-Git Commit ---

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -3,4 +3,10 @@
 @section('content')
 
 <h1>creer un nouveau post</h1>
-@endsection
+
+<form method="POST" action="#">
+    <input type="text" name="title" class=" border-gray-600 border-2 " id="">
+    <textarea name="content" id="" cols="30" rows="10"></textarea>
+    <button type="submit"  class="bg-green-500">creer</button>
+
+    @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <ul>
     <li> <a href="{{ route('welcome')}}">L'accueil</a></li>
-    <li> <a href={{  route(posts.create)}}">creer un nouveau post</a></li>
+    <li> <a href="{{  route('posts.create')}}">creer un nouveau post</a></li>
 <li> <a href="/contact">Page de contact</a></li>
 </ul>
